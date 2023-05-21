f3e9872b82af956a4bbae894307d23da7d608c19
senior9 <ovi215188@gmail.com>
2023-05-21 14:02:24 +0600

    data load

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from './Banner/Banner';
 import Gallery from './Gallery/Gallery';
 import { Outlet } from 'react-router-dom';
 import Categories from '../Categories/Categories';
+import CarToys from '../CarToys/CarToys';
 
 const Home = () => {
     return (
@@ -10,6 +11,7 @@ const Home = () => {
             <Banner></Banner>
             <Gallery></Gallery>
             <Categories></Categories>
+            <CarToys></CarToys>
             <Outlet></Outlet>
 
         </div>
---
+++ src/Pages/SingleToys/SingleToy.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./SingleToy.css"
+
+const SingleToy = ({ SingleToy }) => {
+  console.log(SingleToy.picture);
+  const {toy_name,available_quantity,picture,price,rating} =SingleToy
+  return (
+    <div className="">
+      
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img className="custom-image"
+            src={picture}
+            alt="Shoes"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-black">Toy Name : {toy_name}</h2>
+          <p className="text-black"> Price: {price}</p>
+          <p className="text-black"> Rating: {rating}</p>
+          <div className="card-actions justify-end">
+            <button className="btn custom-btn btn-outline">View Details</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleToy;
